f22b585585854d8b2e8c04b6eb011992861a5ad7
blackout358 <32801563+blackout358@users.noreply.github.com>
2023-07-11 20:35:32 +0100

    add custom appbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/screens/main_screen.dart';
+import 'package:tictactoe/widgets/appbar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: CustomAppBar(),
         body: MainScreen(),
       ),
     );
---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  CustomAppBar({Key? key})
+      : preferredSize = Size.fromHeight(kToolbarHeight),
+        super(key: key);
+
+  @override
+  final Size preferredSize; // default is 56.0
+
+  @override
+  _CustomAppBarState createState() => _CustomAppBarState();
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: Icon(Icons.gamepad_rounded),
+      title: const Text("Tic Tac Toe"),
+      centerTitle: true,
+    );
+  }
+}
